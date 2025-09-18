3cc7c92242f866f92afa0e1ecea62a7669295fe0
yugantar.badhan <yugantar.badhan@wolterskluwer.com>
2025-09-18 16:34:43 +0530

    update properties file

--- src/main/java/com/yugantar/portfolio/PortfolioApplication.java
+++ src/main/java/com/yugantar/portfolio/PortfolioApplication.java
@@ -1,10 +1,13 @@
 package com.yugantar.portfolio;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.JvmMetricsAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = { SystemMetricsAutoConfiguration.class })
+@SpringBootApplication(exclude = { SystemMetricsAutoConfiguration.class, TomcatMetricsAutoConfiguration.class,
+		JvmMetricsAutoConfiguration.class })
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
