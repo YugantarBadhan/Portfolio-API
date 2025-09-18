84068f6cce516babfd62b6c3282e747741c0fa93
yugantar.badhan <yugantar.badhan@wolterskluwer.com>
2025-09-18 16:20:26 +0530

    exclude the SystemMetricsAutoConfiguration

--- src/main/java/com/yugantar/portfolio/PortfolioApplication.java
+++ src/main/java/com/yugantar/portfolio/PortfolioApplication.java
@@ -1,13 +1,13 @@
 package com.yugantar.portfolio;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SystemMetricsAutoConfiguration.class })
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
-
 }
